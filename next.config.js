385dfe02261646865d55eafb45e403bee14fc6f9
satojin <seiron1212@outlook.jp>
2022-03-16 18:11:01 +0900

    netlifyCMSの追加

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (cfg) => {
+    cfg.module.rules.push({
+      test: /\.md$/,
+      loader: "frontmatter-markdown-loader",
+      options: { mode: ["react-component"] },
+    });
+    return cfg;
+  },
+};
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,13 @@ import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
+import { Component } from "react";
+import { attributes, react as HomeContent } from "../content/home.md";
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
+  // const allPostsData = getSortedPostsData();
+  let { title, cats, date } = attributes;
+  const allPostsData = { title, cats, date };
   return {
     props: {
       allPostsData,
@@ -24,8 +28,10 @@ const Home: NextPage = ({ allPostsData }: any) => {
   return (
     <Layout home>
       <Head>
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         <title>{siteTitle}</title>
-      </Head>
+      </Head>{" "}
       <section className={utilStyles.headingMd}>
         <p>こんにちは、私の名前は佐藤仁です。好きな食べ物は鯖の味噌煮です。</p>
         <p>
@@ -33,10 +39,21 @@ const Home: NextPage = ({ allPostsData }: any) => {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      <article>
+        <h1>{allPostsData.title}</h1>
+        <HomeContent />
+        <ul>
+          {allPostsData.cats.map((cat: any, k: any) => (
+            <li key={k}>
+              <h2>{cat.name}</h2>
+              <p>{cat.description}</p>
+            </li>
+          ))}
+        </ul>
+      </article>
       {/* Keep the existing code here */}
-
       {/* Add this <section> tag below the existing <section> tag */}
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      {/* <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }: PostType) => (
@@ -49,7 +66,7 @@ const Home: NextPage = ({ allPostsData }: any) => {
             </li>
           ))}
         </ul>
-      </section>
+      </section> */}
     </Layout>
   );
 };
